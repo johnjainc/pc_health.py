d068bdf696f3575f3d4c904dba1315f031dfc406
irfanrasheedkc <97159197+irfanrasheedkc@users.noreply.github.com>
2022-06-20 18:50:08 +0530

    Add files via upload

---
+++ health.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import shutil
+import psutil
+def check_disk_usage(disk):
+    du=shutil.disk_usage(disk)
+    free=du.free/du.total*100
+    return free
+def check_cpu_usage():
+    usage=psutil.cpu_percent(0.1)
+    return usage
+if check_disk_usage("/")<50 and check_cpu_usage()<50:
+    print("Everything is okay!")
+else:
+    print("ERROR!!")
