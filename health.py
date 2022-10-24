b622a04483dcc0e506502b095e98ea7c699b6de4
Arjun <93950622+Arjun-A-I@users.noreply.github.com>
2022-10-24 12:37:15 +0530

    Update health.py

--- health.py
+++ health.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 import shutil
 import psutil
+#Function to check Disk usage
 def check_disk_usage(disk):
     du=shutil.disk_usage(disk)
     free=du.free/du.total*100
     return free
+
+#Function to check CPU usage
 def check_cpu_usage():
     usage=psutil.cpu_percent(0.1)
     return usage
+
 if check_disk_usage("/")<50 and check_cpu_usage()<50:
     print("Everything is okay!")
 else:
-    print("ERROR!!")
+    print("ERROR!!!!")
